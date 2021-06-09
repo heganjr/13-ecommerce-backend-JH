719431733ecd6959cc5e0abe4c308b869a2e3823
heganjr <jacobhegan@hotmail.com>
2021-06-10 01:14:31 +0800

    models updated Categories get routes completed

--- models/index.js
+++ models/index.js
@@ -5,10 +5,21 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  // entry point - Product belongs to Category and the category id is what links the two tables to each other
+  // PRODUCT CAN ONLY BELONG TO ONE CATEGORY
+
+});
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  // Category has many products and it is linked to products via category_id
+  // I am telling products that you will be an array with multiple objects!
+
+});
+
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
@@ -22,7 +33,7 @@ Product.belongsToMany(Tag, {
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: "tag_id",
-  // Tag belongs to many Products through ProductTag - foreign keys
+  // Tag belongs to many Products through ProductTag - foreign
 });
 // Foreign Key setup
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,12 +3,46 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  // can use async
   // find all categories
+  try {
+    const findAllCategory = await Category.findAll({
+      include: [{ model: Product }],
+      
+    });
+    console.log(findAllCategory)
+    res.status(200).json(findAllCategory);
+    
+  } catch (error) {
+    res.status(500).json(err);
+    
+  }
+  // find all, include the Product model and push JSON of findAllcategory on 200 and err on 500
+
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const findOneID = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+     if (!findOneID) {
+       res.status(404).json({ message: "No category found with that id!" });
+       return;
+      //  I DONT WANT TO DO THE NEXT BIT!
+
+    }
+    res.status(200).json(findOneID);
+    
+  } catch (error) {
+    res.status(500).json(err);
+
+    
+  }
+  // anything 
   // find one category by its `id` value
   // be sure to include its associated Products
 });
